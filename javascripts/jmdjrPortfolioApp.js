0e593ed5b828f30e8cdf9a4aa2595f32c2028446
jmdjr <jmdcodejunkie@gmail.com>
2015-02-18 10:48:30 -0800

    Switching gears one last time
    it appears that the navigation scheme I came up with from AngularJS is
    exactly what I wanted, and I was just getting frustrated.  About Us page
    has mostly been implemented, with its tabs and content displaying. Next
    is to get my Projects section implemented.  animations and fancy-ness
    will come later.

--- javascripts/jmdjrPortfolioApp.js
+++ javascripts/jmdjrPortfolioApp.js
@@ -1,11 +1,10 @@
-var jmdjrPortfolioApp = angular.module('jmdjrPortfolioApp',['ngRoute', 'ngAnimate']);
+var jmdjrPortfolioApp = angular.module('jmdjrPortfolioApp',['ngRoute', 'ngAnimate', 'ui.bootstrap']);
 
 jmdjrPortfolioApp.config(function ($routeProvider){
-    $routeProvider
-        .when('/', 
+        $routeProvider.when('/', 
 			{
 				controller: 'AppController',
-				templateUrl: 'Pages/Navigation.html'
+				templateUrl: 'Pages/Home.html'
 			})
 		.when('/AboutMe',
 			{
@@ -17,19 +16,3 @@ jmdjrPortfolioApp.config(function ($routeProvider){
 
 jmdjrPortfolioApp.controller('AppController', function($scope) {
 });
-
-//jmdjrPortfolioApp.controller('AboutMeController', function($scope) {
-//    
-//});
-//
-//jmdjrPortfolioApp.controller('ProjectsController', function($scope) {
-//    
-//});
-//
-//jmdjrPortfolioApp.controller('WorkHistoryController', function($scope) {
-//    
-//});
-//
-//jmdjrPortfolioApp.controller('SkillsController', function($scope) {
-//    
-//});
