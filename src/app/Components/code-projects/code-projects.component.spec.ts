0ed91ebd9e58f042ff687426864d38fa8940f36e
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2023-12-18 22:29:08 -0800

    fixes and stylin.

---
+++ src/app/Components/code-projects/code-projects.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CodeProjectsComponent } from './code-projects.component';
+
+describe('CodeProjectsComponent', () => {
+  let component: CodeProjectsComponent;
+  let fixture: ComponentFixture<CodeProjectsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CodeProjectsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CodeProjectsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Components/code-projects/code-projects.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-projects',
+  templateUrl: './code-projects.component.html',
+  styleUrls: ['./code-projects.component.css']
+})
+export class CodeProjectsComponent {}
